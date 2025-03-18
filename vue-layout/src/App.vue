61bd13073f0ef480c8ceee441c651d83c9f221ff
Stasyaosan <smavrin38@gmail.com>
2025-03-18 17:20:52 +0300

    расписание выводится

---
+++ vue-layout/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Просмотр расписания</router-link>
+      <router-link to="/editor">Редактировать расписание</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+nav {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+</style>
